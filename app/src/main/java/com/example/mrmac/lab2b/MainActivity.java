963a140118ebca65aa1627ec59592322629fa961
szikanova <szikanova@gmail.com>
2015-02-16 19:48:51 -0500

    buttons exist

--- app/src/main/java/com/example/mrmac/lab2b/MainActivity.java
+++ app/src/main/java/com/example/mrmac/lab2b/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +13,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button testing = (Button)findViewById(R.id.testing);
+        //testing.setOnClickListener();
+        Button training=(Button)findViewById(R.id.training);
+        //training.setOnClickListener();
     }
 
 
