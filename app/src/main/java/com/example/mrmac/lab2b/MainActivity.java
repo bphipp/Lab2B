3f545b5fa66cc1274505355f9bc66134e03e0c09
szikanova <szikanova@gmail.com>
2015-02-24 14:31:53 -0500

    have everything in from lab2a set up

--- app/src/main/java/com/example/mrmac/lab2b/MainActivity.java
+++ app/src/main/java/com/example/mrmac/lab2b/MainActivity.java
@@ -8,20 +8,33 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.graphics.Bitmap;
+import android.os.Environment;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import org.opencv.core.*;
+import org.opencv.objdetect.CascadeClassifier;
 
 
 public class MainActivity extends ActionBarActivity implements CvCameraViewListener2{
     private CameraBridgeViewBase mOpenCvCameraView;
     int option = 0;
+    private Mat grayscaleImage;
+    private int absoluteFaceSize;
+    private CascadeClassifier cascadeClassifier;
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
@@ -101,23 +114,83 @@ public class MainActivity extends ActionBarActivity implements CvCameraViewListe
     }
 
     public void onCameraViewStarted(int width, int height) {
+        grayscaleImage = new Mat(height, width, CvType.CV_8UC4);
+
+
+        // The faces will be a 20% of the height of the screen
+        absoluteFaceSize = (int) (height * 0.2);
     }
 
     public void onCameraViewStopped() {
     }
+    public Mat captureImages(Mat aInputFrame) {
+        Imgproc.cvtColor(aInputFrame, grayscaleImage, Imgproc.COLOR_RGBA2RGB);
 
+
+        MatOfRect faces = new MatOfRect();
+
+
+        // Use the classifier to detect faces
+        if (cascadeClassifier != null) {
+            cascadeClassifier.detectMultiScale(grayscaleImage, faces, 1.1, 2, 2,
+                    new Size(absoluteFaceSize, absoluteFaceSize), new Size());
+        }
+
+
+        // If there are any faces found, draw a rectangle around it
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i <facesArray.length; i++)
+            Core.rectangle(aInputFrame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0, 255), 3);
+
+
+        return aInputFrame;
+    }
+
+    public void saveToFolder(File file, Mat input){
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            Bitmap bmp = Bitmap.createBitmap(input.cols(),  input.rows(),Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(input,bmp);
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
         // Do signal processing here.
         Mat current_frame = inputFrame.rgba();
-        if (option == 2){
-            // 1 -> blur
-//            current_frame = inputFrame.rgba();
+        Mat current_face;
+        if (option == 1){
+            for(int i=0; i<8; i++){
+                //this finds the face and returns a face matrix
+                current_face = captureImages(current_frame);
+                //this is kind of weird still
+                File fileName=new File(Environment.DIRECTORY_PICTURES, "name it some shit");
+                saveToFolder(fileName, current_face);
+                //now i want to save things to training folder
+            }
+            // 1 -> train
             org.opencv.core.Size s = new Size(10,10);
             Imgproc.blur(current_frame, current_frame, s);
 
-        } else if (option == 1){
-//            current_frame = inputFrame.gray();
+        } else if (option == 2){
+            for(int i=0; i<4; i++) {
+                current_face=captureImages(current_frame);
+                File fileName=new File(Environment.DIRECTORY_PICTURES, "name it some shit");
+                saveToFolder(fileName, current_face);
+            }
+//          want to test
             Imgproc.cvtColor(current_frame,current_frame,Imgproc.COLOR_RGB2GRAY);
             Imgproc.Canny(current_frame,current_frame,100,300);
             Imgproc.cvtColor(current_frame,current_frame,Imgproc.COLOR_GRAY2RGB);
